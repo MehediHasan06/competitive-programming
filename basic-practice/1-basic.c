097913f6cf267c28a494bbdc65329167700ae206
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-20 16:21:40 +0600

    cp basics with c

---
+++ basic-practice/1-basic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//int main()
+//{
+//    printf("Hello World\n");
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 50, b = 60, sum;
+//    sum = a + b;
+//    printf("Sum = %d\n", sum);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    double x = 10.5;
+//    int a = (int)x; //Type cast to integar from double
+//    printf("x = %lf & a = %d\n", x, a); // output: x=10.50000 & a=10
+//    return 0;
+//}
+
+
