fea781956eabacc5662e434b941a996ee9deb0fd
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-05-11 13:11:11 +0600

    bulb, hide and show the header

--- dom-practice/2. basic-dom-js/1.basicDom/script.js
+++ dom-practice/2. basic-dom-js/1.basicDom/script.js
@@ -2,7 +2,8 @@ let body = document.querySelector("body");
 
 // header
 let header = document.createElement("h3");
-header.innerHTML = "If you want to see what js can do, please try the below button !"
+header.innerHTML = "If you want to see what js can do, please try the below button !";
+header.setAttribute("id", "header-id");
 
 // button & paragraph
 let button = document.createElement("button");
@@ -22,7 +23,7 @@ body.appendChild(header);
 body.appendChild(button);
 body.appendChild(lineBreaker);
 
-// Bulb project ----------------------------
+// Bulb project ---------------------------------
 
 // bulb on button
 let onButton = document.createElement("button");
@@ -55,3 +56,50 @@ offButton.addEventListener("click", () => {
   bulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
 });
 
+
+// Button to change the style of the header  ------------------------------
+let styleParagraph = document.createElement("p");
+styleParagraph.innerText = "Button to change the css style";
+body.appendChild(styleParagraph);
+
+let styleButton = document.createElement("button");
+styleButton.type = "button";
+styleButton.innerText = "Change the style of the header";
+styleButton.addEventListener("click", () => {
+  let header = document.getElementById("header-id");
+  header.style.color = "red";
+  header.style.textAlign = "center";
+});
+
+
+body.appendChild(styleButton);
+
+// Button to hide any html element  -------------------------
+let hideParagraph = document.createElement("p");
+hideParagraph.innerText = "Button to hide the html element";
+body.appendChild(hideParagraph);
+
+let hideButton = document.createElement("button");
+hideButton.type = "button";
+hideButton.innerText = "Hide the element of html";
+hideButton.addEventListener("click", () => {
+  let header = document.getElementById("header-id");
+  header.style.display = "none";
+});
+
+body.appendChild(hideButton);
+
+// Button to show any hidden html element  -------------------
+let showParagraph = document.createElement("p");
+showParagraph.innerText = "Button to hide the html element";
+body.appendChild(showParagraph);
+
+let showButton = document.createElement("button");
+showButton.type = "button";
+showButton.innerText = "Show the element of html";
+showButton.addEventListener("click", () => {
+  let header = document.getElementById("header-id");
+  header.style.display = "block";
+});
+
+body.appendChild(showButton);
