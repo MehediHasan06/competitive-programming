53aa42f576b7bd168af07fc79de2818a807f8606
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-06-03 15:15:27 +0600

    code refactored 2

--- javascript/w3resource/2.timeFormat.js
+++ javascript/w3resource/2.timeFormat.js
@@ -2,9 +2,13 @@
  * Write a JavaScript program to get the current date.
  */
 
-const currentTime = new Date();
-const year = currentTime.getFullYear();
-const month = currentTime.getMonth() + 1; //It starts counting from 0 to 11.
-const date = currentTime.getDate();
+const currentDate = () => {
+  const currentTime = new Date();
+  const year = currentTime.getFullYear();
+  const month = currentTime.getMonth() + 1; //It starts counting from 0 to 11.
+  const date = currentTime.getDate();
 
-console.log(`${month}/${date}/${year}`);
+  return `${month}/${date}/${year}`;
+}
+
+console.log(currentDate());
