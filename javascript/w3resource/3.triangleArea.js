b66f971ed26d7500fd36fec46bd0f10a65d6ffc2
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-06-03 15:21:27 +0600

    code refactored 3

--- javascript/w3resource/3.triangleArea.js
+++ javascript/w3resource/3.triangleArea.js
@@ -2,12 +2,11 @@
  * Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
  */
 
-const a = 5;
-const b = 6;
-const c = 7;
+ const triangleArea = (a, b, c) => {
+  // s for semiperimeter
+  const s = (a + b + c) / 2;
+  const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  return area;
+}
 
-// s for semiperimeter
-const s = (a + b + c) / 2;
-const sum = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-
-console.log(sum);
+console.log(triangleArea(5,6,7));
