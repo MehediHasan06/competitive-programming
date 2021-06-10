b9c774cd813b9b27216aee4f82e9d28a29532f0c
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-06-10 15:15:25 +0600

    chirstmas problem is solved

--- javascript/w3resource/8.tillChristmas.js
+++ javascript/w3resource/8.tillChristmas.js
@@ -1,9 +1,16 @@
+/**
+ **  to calculate days left until next Christmas
+ */
+
 const daysLeft = () => {
   var today = new Date().getTime();
-  var christmas = new Date(`12/25/${new Date().getFullYear()}`).getTime();
-  // var remainDays = Math.floor(((christmas-today)/(1000*3600*24))+0.5); 
-  var remainDays = (christmas-today)/(1000*3600*24);
+  if(new Date().getMonth() == 11 && new Date().getDate() > 25){
+    var christmas = new Date(`12/25/${new Date().getFullYear() + 1}`).getTime();  
+  } else {
+    var christmas = new Date(`12/25/${new Date().getFullYear()}`).getTime();  
+  }
 
+  var remainDays = Math.ceil(((christmas-today)/(1000*3600*24))); 
   return remainDays;
 }
 
